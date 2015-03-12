2e013e956b98e4165089e1a04ee64918da7d1409
unknown <William.james.lawson@ADMINISTRATOR44.win.lithgow-h.schools.nsw.edu.au>
2015-03-12 14:29:06 +1100

    Commit (2) (Had to restart coding, added initial setup, startup, and menu. Encounter no issues.)

--- FunWithMarksFolder/Assessment1/Module1.vb
+++ FunWithMarksFolder/Assessment1/Module1.vb
@@ -10,7 +10,7 @@
 
     Dim Pro1 As New Profiles
 
-    Sub Menu()
+    Sub Startup()
 
         'Startup
         Console.SetCursorPosition(23, 0)
@@ -19,14 +19,14 @@
         Console.WriteLine("Your all in one lawn management system.")
         Console.SetCursorPosition(25, 4)
         Console.WriteLine("Press any key to continue...")
-        Console.ReadLine()
+        Console.ReadKey(True)
 
         'Change Later for Profiles
         Console.Clear()
         Console.WriteLine("No company information has been found, We'll set up a profile before we begin.")
         Console.WriteLine()
         Console.WriteLine("Press any key to continue...")
-        Console.ReadKey()
+        Console.ReadKey(True)
 
 
     End Sub
@@ -38,23 +38,115 @@
         'Setting Up the profile
         Console.WriteLine("Here you need to enter the details for your new company profile.")
         Console.WriteLine()
-        Console.WriteLine("Company Name: ")
+        Console.Write("Company Name: ")
         Pro1.CName = Console.ReadLine
-        Console.WriteLine("Company Owner's Name: ")
+        Console.Write("Company Owner's Name: ")
         Pro1.COwner = Console.ReadLine
-        Console.WriteLine("Company Contact number: ")
+        Console.Write("Company Contact number: ")
         Pro1.CNumber = Console.readline
-        Console.WriteLine("Company Address: ")
+        Console.Write("Company Address: ")
         Pro1.CAddress = Console.ReadLine
-        Console.WriteLine("Company Hourly Rate: ")
+        Console.Write("Company Hourly Rate: ")
         Pro1.Rate = Console.ReadLine
+        Console.WriteLine()
+        Console.WriteLine("Setup is Complete!")
+        Coms()
+    End Sub
+
+    Sub Menu()
+
+        Dim selection As Char
+
+        Do
+
+            'Draw up the menu
+            Console.WriteLine("Welcome to Fun With Marks, the most mark calculating program")
+            Console.WriteLine()
+            Console.WriteLine(" (A) Add a booking")
+            Console.WriteLine(" (B) View all incomplete bookings")
+            Console.WriteLine(" (C) View all complete booking")
+            Console.WriteLine(" (D) Check incomplete bookings for next 7 days")
+            Console.WriteLine()
+            Console.WriteLine(" (E) View incomplete booking details")
+            Console.WriteLine(" (F) Edit incomplete booking details")
+            Console.WriteLine()
+            Console.WriteLine(" (G) Remove a booking")
+            Console.WriteLine(" (H) Complete a booking")
+            Console.WriteLine()
+            Console.WriteLine(" (I) View business card")
+            Console.WriteLine()
+            Console.WriteLine(" (X) Exit")
+            Console.Write("Selection: ")
+
+            'Get the selected letter from the user
+            selection = Console.ReadKey(True).KeyChar.ToString.ToUpper
+
+            Select Case selection
+                Case "A"
+                    AddBookings()
+                Case "B"
+                    AllIncomplete()
+                Case "C"
+                    AllComplete()
+                Case "D"
+                    SevenDaysIncomplete()
+                Case "E"
+                    ViewIncomplete()
+                Case "F"
+                    EditIncomplete()
+                Case "G"
+                    RemoveBooking()
+                Case "H"
+                    CompleteBooking()
+                Case "I"
+                    BusinessCard()
+            End Select
+
+        Loop Until selection = "X"
 
     End Sub
+
+    Sub AddBookings()
+
+    End Sub
+
+    Sub AllIncomplete()
+
+    End Sub
+
+    Sub AllComplete()
+
+    End Sub
+
+    Sub SevenDaysIncomplete()
+
+    End Sub
+
+    Sub ViewIncomplete()
+
+    End Sub
+
+    Sub EditIncomplete()
+
+    End Sub
+
+    Sub RemoveBooking()
+
+    End Sub
+
+    Sub CompleteBooking()
+
+    End Sub
+
+    Sub BusinessCard()
+
+    End Sub
+
     Sub Main()
 
-        Menu()
+        Startup()
         ProfileSetup()
-
+        Menu()
     End Sub
 
 End Module
